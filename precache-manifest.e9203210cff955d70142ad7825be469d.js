bc923dae8e8c1471d26d20470a77caa71f3a94ad
loren-garcia <loren.garcia@live.com>
2020-09-14 11:35:36 -0300

    Updates

---
+++ precache-manifest.e9203210cff955d70142ad7825be469d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2c8673d27147ebf00298b22bd0a47e8",
+    "url": "/pjw2/index.html"
+  },
+  {
+    "revision": "2afb6aa30de34234db68",
+    "url": "/pjw2/static/css/main.24043c55.chunk.css"
+  },
+  {
+    "revision": "99071ff14f4c4a8cef3d",
+    "url": "/pjw2/static/js/2.3158a166.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/pjw2/static/js/2.3158a166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2afb6aa30de34234db68",
+    "url": "/pjw2/static/js/main.76a84efa.chunk.js"
+  },
+  {
+    "revision": "2eb4b3712cd78a379994",
+    "url": "/pjw2/static/js/runtime-main.bc7b1a37.js"
+  },
+  {
+    "revision": "84f3ce0d00345e0c38f291ed62de50b6",
+    "url": "/pjw2/static/media/Kubernetes_Logo2.84f3ce0d.svg"
+  },
+  {
+    "revision": "6a347940f064146525be36b805414901",
+    "url": "/pjw2/static/media/favicon.6a347940.png"
+  },
+  {
+    "revision": "7f0f182677c5df6756b17fdb790a563b",
+    "url": "/pjw2/static/media/flower.7f0f1826.png"
+  },
+  {
+    "revision": "40675b0b2cdd8f5628e92e856bb99a26",
+    "url": "/pjw2/static/media/texture.40675b0b.png"
+  }
+]);
